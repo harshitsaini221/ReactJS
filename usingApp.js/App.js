c34259a6b7c40c56e410a50685e9eb8b6e374b1a
harshitsaini221 <harshitsaini221git config --global user.name harshitsaini221>
2023-06-13 18:42:15 +0530

    Explainations Added

--- usingApp.js/App.js
+++ usingApp.js/App.js
@@ -1,6 +1,13 @@
-const heading = React.createElement("h1", {id: "heading"}, "Hello World From React!");
+const heading = React.createElement(
+  "h1", //tag
+  { id: "heading" }, //attributes
+  "Hello World From React!" //children
+);
 
 //object is where we give attributes to our tags
+
+console.log(heading);
+//react element (JS object), has props
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
