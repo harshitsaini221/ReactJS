09b5bcc6c91abcfcac3e0a1cfc83187f5bd8d151
harshitsaini221 <harshitsaini221git config --global user.name harshitsaini221>
2023-06-13 19:02:29 +0530

    Understanding React

---
+++ Sibling Tag/App.js
@@ -0,0 +1,16 @@
+//for adding sibling tag
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am heading Tag"),
+    React.createElement("h2", {}, "I am heading 2 Tag"),
+  ])
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
--- usingApp.js/App.js
+++ usingApp.js/App.js
@@ -10,4 +10,9 @@ console.log(heading);
 //react element (JS object), has props
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(heading);
+
+//render takes the React Element which is a JS Object,
+//translates is to a h1 tag which the browser understands
+//and put it in the root element.
