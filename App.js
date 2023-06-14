44085ea67b294d6e11c1a529760588126ec3be24
harshitsaini221 <harshitsaini221git config --global user.name harshitsaini221>
2023-06-14 13:53:03 +0530

    NPM

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I am heading Tag"),
+      React.createElement("h2", {}, "I am heading 2 Tag"),
+    ])
+  );
+  
+  console.log(parent);
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+  root.render(parent);
