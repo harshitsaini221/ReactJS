723943753e23186ccde96c4322b1bdc6e0741738
harshitsaini221 <harshitsaini221git config --global user.name harshitsaini221>
2023-06-15 18:51:47 +0530

    Food App Header & Body

--- App.js
+++ App.js
@@ -1,48 +1,106 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {FeedRocketIcon} from "react-icons"
 
-//React Element
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Harshit Saini ReactJS"
-// );
-
-//JSX -> HTML-like or XML-like
-const Title = () => (
-  <h1 id="heading" tabIndex="5">
-    JSX Heading!
-  </h1>
-);
-
-const element = <h1>This is a React Element</h1>;
-const number = 22011999;
-
-//React Functional Component
-//we cannot have 2 root containers in jSX,
-//either we shall wrap it in a <div> tag
-//or use React.Fragment Tag, which basically behaves as an empty tag
-
-const HeadingComponent = () => {
+/**
+ * Header
+ * - Logo
+ * - Nav Items
+ *
+ * Body
+ * - Search
+ * - Restaurant Conatiner
+ *      - Restaurant Card
+ *
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ *
+ */
+const Header = () => {
   return (
-    <React.Fragment>
-    <div id="container">
-      <Title />
-      {element}
-      <h2>{number}</h2>
-      <h1 className="heading">React Functional Component</h1>
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-    <div id="container2">
-
-    </div>
-    </React.Fragment>
   );
 };
 
-// similar to const HeadingComponent = () => <h1 className="heading">React Functional Component</h1>;
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rfngxpkeablm4ses3d0j"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 Stars</h4>
+      <h4>38 mins</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
